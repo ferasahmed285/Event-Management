3d738e7dc4ffd31f1878d26399a1de30677fabc5
samaelqadi <sama.tarek967@gmail.com>
2025-04-22 03:04:22 +0400

    test

--- src/Validation.java
+++ src/Validation.java
@@ -1,57 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
+public interface Validation {
 
-public class Validation {
-    private List<String> usernames = new ArrayList<>();
+    boolean hasSufficientBalance(Attendee attendee, double amount);
 
-    public class Instructor {
-        Wallet wallet;
+    boolean isRoomCapacityValid(Room room, int numberOfGuests);
 
-        public Instructor(double balance) {
-            this.wallet = new Wallet(balance);
-        }
+    boolean isValidUsername(String username);
 
-        public Wallet getWallet() {
-            return wallet;
-        }
+    boolean isValidPassword(String password);
 
-        public class Wallet {
-            private double balance;
-
-            public Wallet(double balance) {
-                this.balance = balance;
-            }
-
-            public double getBalance() {
-                return balance;
-            }
-        }
-    }
-
-    public boolean hasSufficientBalance(Instructor instructor, double amount) {
-        return instructor.wallet.getBalance() >= amount;
-    }
-
-    public boolean isValidUsername(String username) {
-        if (username == null || username.length() <= 0) return false;
-        for (int i = 0; i < username.length(); i++) {
-            char ch = username.charAt(i);
-            if (!Character.isLetterOrDigit(ch) && ch != '_') return false;
-        }
-        return !usernames.contains(username);
-    }
-
-    public boolean isValidPassword(String password) {
-        return password != null && password.length() >= 8;
-    }
-
-    public void addUsername(String username) {
-        if (isValidUsername(username)) {
-            usernames.add(username);
-        }
-    }
-
-    public List<String> getUsernames() {
-        return usernames;
-    }
+    void addUsername(String username);
 }
