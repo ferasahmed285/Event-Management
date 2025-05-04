104ff481cbf00658864032de14cd8a9830b6bedd
ferasahmed285 <feras.ahmed1@yahoo.com>
2025-05-04 15:14:10 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class Main{
-    public static void main(String[] args) {
-            Database.initializeDummyData();
-    }
-}
+//public class Main{
+//    public static void main(String[] args) {
+//            Database.initializeDummyData();
+//    }
+//}
