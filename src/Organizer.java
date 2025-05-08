d145b4efe558ae2ca801e186164462c5b180a2aa
Ragab26 <Ahmed.Ragab2615@gmail.com>
2025-05-08 21:10:55 +0300

    Update Organizer.java

--- src/Organizer.java
+++ src/Organizer.java
@@ -1,11 +1,14 @@
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
